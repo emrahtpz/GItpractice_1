1dddb430f7c6b4565988239465f7c21e102371da
EmrahTopuz <emrahtopuz@yahoo.com>
2022-11-18 01:19:01 +0300

    added more work after publishing

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
 
         System.out.println("Hello World");
         System.out.println("Hello entire world");
+        System.out.println("Stuffs on are on github");
     }
 }
